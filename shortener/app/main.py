1404aeb8d9c2a8dc6d2182c6c3c678614bf1c2a3
OmerKriger <omerkriger@gmail.com>
2025-06-02 16:22:35 +0300

    Add Custom URL option to Create Request

--- shortener/app/main.py
+++ shortener/app/main.py
@@ -1,12 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+import shortuuid
+import re
 
 from infrastructure.cache import get_cached_original_code, cache_short_url
 from infrastructure.db import connect_db, disconnect_db, save_url_mapping, get_long_url
-import shortuuid
 
 app = FastAPI()
+ALIAS_PATTERN = re.compile(r'^[a-zA-Z0-9\-]{6,30}$')
 
 @app.on_event("startup")
 async def startup():
@@ -22,44 +25,74 @@ async def shutdown():
 
 class URLRequest(BaseModel):
     url: str
+    alias: Optional[str] = None
 
 def generate_short_code(length=6):
-    short_code = shortuuid.ShortUUID().random(length=6)
+    short_code = shortuuid.ShortUUID().random(length=length)
     return short_code
+
+def validate_url_alias(alias: str) -> bool:
+    return ALIAS_PATTERN.match(alias) is not None
 
 @app.post("/shorten")
 async def create_short_url(data: URLRequest):
     full_url = data.url
+    alias = data.alias
+
     print(f"\n🔗 Request to shorten: {full_url}")
 
-    # Attempt to find a unique short URL
-    while True:
-        short_url = generate_short_code()
-        print(f"🔍 Trying short URL: {short_url}")
+    if alias:
+        print(f"🔍 Requested custom alias: {alias}")
 
-        # Step #3 - Check Redis
-        if await get_cached_original_code(short_url):
-            print("⚠️ Short URL exists in cache. Retrying...")
-            continue
+        if not validate_url_alias(alias):
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST, 
+                detail="Invalid custom alias format, Must be 6-30 characters long and contain only letters, numbers"
+                )
+        
+        # Check if alias is already taken
+        if await get_long_url(alias) or await get_cached_original_code(alias):
+            raise HTTPException(
+                status_code=HTTP_409_CONFLICT,
+                detail="Custom alias is already taken"
+                )
+        
+        short_url = alias
+    else:
+        # Attempt to find a unique short URL
+        while True:
+            short_url = generate_short_code()
+            print(f"🔍 Trying short URL: {short_url}")
 
-        # Step #4 - Check DB
-        if await get_long_url(short_url):
-            print("⚠️ Short URL exists in DB. Retrying...")
-            continue
+            # Step #3 - Check Redis
+            if await get_cached_original_code(short_url):
+                print("⚠️ Short URL exists in cache. Retrying...")
+                continue
 
-        break  # Short URL is unique
+            # Step #4 - Check DB
+            if await get_long_url(short_url):
+                print("⚠️ Short URL exists in DB. Retrying...")
+                continue
+
+            break  # Short URL is unique
 
     try:
-        await save_url_mapping(short_code=short_url,
-                               long_url=full_url)
+        await save_url_mapping(short_code=short_url, long_url=full_url)
         print(f"✅ Inserted into DB: {short_url} -> {full_url}")
 
         await cache_short_url(short_url=short_url,
                               long_url=full_url)
         print(f"🧠 Cached: {short_url} -> {full_url}")
 
-        return {"short_url": f"http://localhost:8000/{short_url}"} # TODO: Change address to dynamic
+        return {
+            "short_url": f"http://localhost:8000/{short_url}", # TODO: Change address to dynamic
+            "original_url": full_url,
+            "alias": short_url
+            } 
 
     except Exception as e:
         print(f"❌ Error during insert: {e}")
-        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail="Internal Server Error"
+            )
